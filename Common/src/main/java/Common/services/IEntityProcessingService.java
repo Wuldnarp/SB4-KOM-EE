7b935e712b64da82c0eacad3fa0faf51b0ed15de
Emre Eker <emeke21@student.sdu.dk>
2023-05-22 20:33:05 +0200

    added javadoc to interfaces

--- Common/src/main/java/Common/services/IEntityProcessingService.java
+++ Common/src/main/java/Common/services/IEntityProcessingService.java
@@ -6,9 +6,11 @@ import Common.data.World;
 public interface IEntityProcessingService {
 
     /**
-     * @
-     * @param gameData
-     * @param world
+     * Actions for every game tick that is required to run in the game loop.
+     * @param gameData logic for the game, ex: delta time and events.
+     * @param world map for entities in the game.
+     * @Pre-condition: a cycle has happened.
+     * @Post-condition: actions described in this cycle, has taken effect before the next.
      */
     void process(GameData gameData, World world);
 }
--- Common/src/main/java/Common/services/IGamePluginService.java
+++ Common/src/main/java/Common/services/IGamePluginService.java
@@ -4,7 +4,22 @@ import Common.data.GameData;
 import Common.data.World;
 
 public interface IGamePluginService {
+
+    /**
+     * entry point for an entity
+     * @param gameData logic for the game, ex: delta time and events.
+     * @param world map for entities in the game.
+     * @Pre-condition: Called in the beginning of the game.
+     * @Post-condition: Entities added to the world map, wil be considered valid entities.
+     */
     void start(GameData gameData, World world);
 
+    /**
+     * deletion of the entity
+     * @param gameData logic for the game, ex: delta time and events.
+     * @param world map for entities in the game.
+     * @Pre-condition: Called when the entity has to be removed from the game.
+     * @Post-condition: Entities removed for the world, will no longer be shown and acted upon.
+     */
     void stop(GameData gameData, World world);
 }
