e55e55d0ed2a3c0fbd5d4225da1d5b3ba32c47f4
Emre Eker <emeke21@student.sdu.dk>
2023-05-30 10:48:34 +0200

    a bit clean up in ProcessInjector

--- Core/src/main/java/Core/injectors/ProcessInjector.java
+++ Core/src/main/java/Core/injectors/ProcessInjector.java
@@ -6,12 +6,18 @@ import Common.services.IEntityProcessingService;
 import Common.util.SPILocator;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Service("ProcessInjector")
 public class ProcessInjector {
     public void runAllProcesses(GameData gameData, World world) {
-        List<IEntityProcessingService> list = SPILocator.locateAll(IEntityProcessingService.class);
+        boolean firstTime = true;
+        List<IEntityProcessingService> list = new LinkedList<>();
+        if (firstTime){
+             list = SPILocator.locateAll(IEntityProcessingService.class);
+             firstTime = false;
+        }
         for(IEntityProcessingService iGamePluginService : list){
             iGamePluginService.process(gameData,world);
         }
